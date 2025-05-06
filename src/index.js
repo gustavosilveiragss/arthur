514e8373108e0afbcd79449bbc5b99c217f72b2d
Gustavo Silveira <gustavo24gss@gmail.com>
2025-05-06 18:55:38 -0300

    a

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import { createScene } from './scene';
-
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.getElementById('scene-container');
-  createScene(container);
-});
